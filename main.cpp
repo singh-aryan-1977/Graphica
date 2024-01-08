f50b9b25fd4dd728d06bc57143779b8140d77528
Aryan Singh <aryan_singh@brown.edu>
2024-01-08 21:58:25 +0700

    Feat: Added logic for hitting sphere using quadratic formula

--- main.cpp
+++ main.cpp
@@ -9,7 +9,19 @@
 
 using namespace std;
 
+bool hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 dist = r.origin()-center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(dist, r.direction());
+    auto c = dot(dist, dist) - (radius*radius);
+    auto d = (b*b)-(4*a*c);
+    return (d >= 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1),0.5, r)){
+        return color(1,0,0);
+    }
     vec3 unit_dir = unit_vector(r.direction());
     auto a = 0.5 * (unit_dir.y()+1.0);
     return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
